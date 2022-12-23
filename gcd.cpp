58e8815d8eb082af6422ae25ecfed85d6365f5ff
ahmed-afzal1 <afzalbhairab4@gmail.com>
2022-12-23 20:30:19 +0600

    find GCD of an array 1979

--- gcd.cpp
+++ gcd.cpp
@@ -7,16 +7,43 @@ int main(){
         freopen("output.txt","w",stdout);
     #endif
 
-    int a,b;
-    cin>>a>>b;
-    int reminder = 0;
+    // int a,b;
+    // cin>>a>>b;
+    // int reminder = 0;
 
-    cout<<a%b;
-    while(b){
-       reminder = a % b;
-        a = b;
-        b = reminder;
+    // cout<<a%b;
+    // while(b){
+    //    reminder = a % b;
+    //     a = b;
+    //     b = reminder;
+    // }
+
+    int n;
+    cin>>n;
+
+    vector<int> nums;
+
+    for(int i = 0; i<n; i++){
+        int x;
+        cin>>x;
+        nums.push_back(x);
     }
+
+    sort(nums.begin(),nums.end());
+
+    int firstValue = nums.back();
+    int secondValue = nums.front();
+    int remainder = 0;
+
+    while (secondValue)
+    {
+        remainder = firstValue % secondValue;
+        firstValue = secondValue;
+        secondValue = remainder;
+    }
+
+    cout<<firstValue;
+    
 
     return 0;
 }
