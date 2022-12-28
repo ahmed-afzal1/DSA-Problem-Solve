acc2b2e8fbab0e2904c3cba11403d9c39bba0b25
ahmed-afzal1 <afzalbhairab4@gmail.com>
2022-12-28 22:16:38 +0600

    cd

---
+++ sortingtechniues/sort-1/tempCodeRunnerFile.cpp
@@ -0,0 +1,10 @@
+    for(int i=1; i<n; i++){
+        int current = arr[i];
+        int j = i-1;
+
+        while(arr[j]>current && j>0){
+            arr[j+1] = arr[j];
+            j--;
+        }
+        arr[j+1] = current;
+    }
