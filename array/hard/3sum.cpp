dcae62ce7d04606d2b03954461e28f4c727eb64b
ahmed-afzal1 <afzalbhairab4@gmail.com>
2023-01-30 00:16:21 +0600

    3sum leetcode-15

---
+++ array/hard/3sum.cpp
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+    #ifndef ONLINE_JUDGE
+        freopen("../../input.txt","r",stdin);
+        freopen("../../output.txt","w",stdout);
+    #endif
+
+    int n;
+    cin>>n;
+
+    vector<int> nums;
+    for(int i=0; i<n; i++){
+        int x;
+        cin>>x;
+        nums.push_back(x);
+    }
+        int target = 0;
+        sort(nums.begin(), nums.end());
+        set<vector<int>> s;
+        vector<vector<int>> output;
+        for (int i = 0; i < nums.size(); i++){
+            int j = i + 1;
+            int k = nums.size() - 1;
+            while (j < k) {
+                int sum = nums[i] + nums[j] + nums[k];
+                if (sum == target) {
+                    s.insert({nums[i], nums[j], nums[k]});
+                    j++;
+                    k--;
+                } else if (sum < target) {
+                    j++;
+                } else {
+                    k--;
+                }
+            }
+        }
+        for(auto triplets : s)
+            output.push_back(triplets);
+
+        for(int i=0; i<output.size(); i++){
+            for(int j=0; j<output[i].size(); j++){
+                cout<<output[i][j]<<" ";
+            }
+            cout<<endl;
+        }
+
+
+    return 0;
+}
