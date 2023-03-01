267783e613cd4dfdf571baafbf5ea18f139f1c13
ahmed-afzal1 <afzalbhairab4@gmail.com>
2023-03-01 20:32:07 +0600

    Minimum Operations to Make the Array Increasing leetcode-1827

---
+++ array/easy/min_operations.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int minOperations(vector<int>& nums) {
+    int c = 0;
+
+    int n=nums.size();
+
+    for(int i=1;i<n;i++){
+        if(nums[i]<=nums[i-1]){
+            c=c+(nums[i-1]-nums[i])+1;
+            nums[i]=nums[i]+(nums[i-1]-nums[i])+1;
+        }
+    }
+    return c;
+}
+
+int main(){
+    #ifndef ONLINE_JUDGE
+        freopen("../../input.txt","r",stdin);
+        freopen("../../output.txt","w",stdout);
+    #endif
+
+    int n;
+    cin>>n;
+
+    vector<int> nums;
+    for(int i=0; i<n; i++){
+        int x;
+        cin>>x;
+        nums.push_back(x);
+    }
+
+    cout<<minOperations(nums);
+
+    return 0;
+}
